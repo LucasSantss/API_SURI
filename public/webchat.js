5c875c727fdce6d57a2d3e658b61b92593d739e2
LucasSantss <lucas.santosrod@hotmail.com>
2025-09-08 23:08:32 -0300

    webchat

---
+++ public/webchat.js
@@ -0,0 +1,16 @@
+window.cbAsyncInit = function () {
+    CBM.ChatbotId = "cb83498626";
+
+    CBM.StartWebChat().then(webChat => {
+
+    }).catch(reason => {
+
+    });
+};
+(function (d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) { return; }
+    js = d.createElement(s); js.id = id;
+    js.src = "https://webchat.chatbotmaker.io/cbm-jssdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'cbm-jssdk'));
