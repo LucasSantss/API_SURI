84e7aef2b3a9eb5317c5163fa29ca6192c52e287
LucasSantss <lucas.santosrod@hotmail.com>
2025-10-27 22:57:05 -0300

    mensagem de erro

--- api/webhooks.js
+++ api/webhooks.js
@@ -27,7 +27,7 @@ export default async function handler(req, res) {
       return res.status(200).json({ success: true, message: "Todos os resultados foram apagados" });
     } catch (err) {
       console.error("Erro ao deletar Resultados:", err);
-      return res.status(500).json({ error: "Erro interno" });
+      return res.status(500).json({ success: false, message: "Erro interno" });
     }
   } else {
     res.setHeader("Allow", ["GET", "DELETE", "OPTIONS"]);
